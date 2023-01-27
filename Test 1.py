3a2afd977867d9769038ad60a5c44e471f2fca68
NadiaRezazadeh <122272736+NadiaRezazadeh@users.noreply.github.com>
2023-01-27 22:44:07 +0330

    Update Test 1.py

--- Test 1.py
+++ Test 1.py
@@ -2,14 +2,18 @@ import random
 n = int(input("How many random non-repeating numbers do you want to receive? "))
 
 list = []
-for i in range(n):
+
+while True:
     a = random.randint(0,500)
-    list.append(a)
-    
-if a in list:
-        list.remove(a)
-        b = random.randint(0,500) 
-        list.append(b)
+   
+    if a in list:
+       continue
+    else:
+        list.append(a)
+
+    if len(list) == n :
+        break      
+         
 
 print("list =" , list)
 
